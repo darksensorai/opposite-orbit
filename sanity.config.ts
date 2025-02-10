626ef7278467296833e1fbc6eba9ece344468540
darksensorai <eb@darksensor.ai>
2025-02-10 17:29:54 -0600

    fds

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,8 @@
-// ./sanity.config.ts
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { schema } from "./src/sanity/schemaTypes";
+import { schema } from "./src/sanity/schema";
 import { presentationTool } from "sanity/presentation";
+import { resolve } from "./src/sanity/lib/resolve";
 
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [
     structureTool(),
     presentationTool({
+	  resolve,
       previewUrl: location.origin,
     }),
   ],
